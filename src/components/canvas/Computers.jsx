4acdb8832df0cf2d9e6db7f58bc80ca5acbf0514
胡赞來 <361206768@qq.com>
2023-05-01 23:37:45 +0800

    finish computer model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import CanvasLoader from '../Loader'
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas
